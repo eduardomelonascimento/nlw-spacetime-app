4831571a140b31dc5e447411ca171a9c692713f7
Eduardo Melo <nascimentomeloedu@gmail.com>
2023-06-06 01:29:11 -0300

    feat: created login callback component and logged profile component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,15 @@
+export default function Footer() {
+  return (
+    <div className="text-sm leading-relaxed text-gray-200">
+      Feito com 💜 por{' '}
+      <a
+        href="https://rocketseat.com.br"
+        target="_blank"
+        className="underline hover:text-gray-100"
+        rel="noreferrer"
+      >
+        Rocketseat
+      </a>
+    </div>
+  )
+}
---
+++ src/lib/auth.ts
@@ -0,0 +1,14 @@
+import decode from 'jwt-decode'
+import { cookies } from 'next/headers'
+
+interface User {
+  sub: string
+  name: string
+  avatarUrl: string
+}
+
+export function getUser(): User {
+  const token = cookies().get('token')?.value
+  if (!token) throw new Error('Unauthenticated')
+  return decode(token)
+}
